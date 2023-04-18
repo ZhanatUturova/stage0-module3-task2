c7e991d8b84e4aa5a3bfbffc2e6aab095af235f4
Zhanat Uturova <526628@gmail.com>
2023-04-18 06:19:34 +0600

    Third module second task

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,19 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private final int ADULT_AGE = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void callToFriend() {
+        //some code
+    }
+
+    void callByNumber(int number) {
+        //some code
+    }
+    public static void main(String[] args) {
+        
+    }
+}
